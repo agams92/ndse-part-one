afcf90c8465491cd7906321f5b650ab268f2c33a
Lev <l.belonovskiy@onpoint.ru>
2021-03-04 00:22:35 +0300

    кто не сохранил файл перед пушем - тот дурак

--- app/routers/index.js
+++ app/routers/index.js
@@ -1,4 +1,5 @@
 const { BookApiRouter, BookRenderRouter } = require('./book');
 const { UserApiRouter } = require('./user');
+const MainRenderRouter = require('./main/router.render');
 
-module.exports = { BookApiRouter, BookRenderRouter, UserApiRouter };
+module.exports = { BookApiRouter, BookRenderRouter, UserApiRouter, MainRenderRouter };
