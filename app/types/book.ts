d1a72931b71ac7656478d31a2fa2b2f4113d0dc9
Lev <agams92@gmail.com>
2021-04-24 08:09:42 +0300

    Revert "добавил BooksRepository"
    This reverts commit fa998a82c7c760ad370f47e937024eb74453fdf1.

--- app/types/book.ts
+++ app/types/book.ts
@@ -8,29 +8,6 @@ interface IBook {
   fileBook: string
 }
 
-class BooksRepository {
-  createBook(book:IBook) {
-    const newBook = new Book().save()
-    return newBook
-  }
-
-  getBook(id:number): IBook{
-    return Book.findById(id)
-  }
-
-  getBooks(): IBook[]{
-    return Book.find();
-  }
-
-  updateBook(id:number) {
-    return Book.findByIdAndUpdate(id, {title: 'lel'})
-  }
-
-  deleteBook(id:number) {
-    return Book.deleteOne({ _id: id })
-  }
-}
-
 export type {
   IBook
 }
